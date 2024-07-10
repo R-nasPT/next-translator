dad9819041b96573a2c18f7cac4087b7eaad947e
Yotsatorn  Pathan <120444604+R-nasPT@users.noreply.github.com>
2024-07-10 10:00:54 +0700

    Update sidebar

--- intl-auth/src/components/layouts/sidebar/index.tsx
+++ intl-auth/src/components/layouts/sidebar/index.tsx
@@ -1,8 +1,10 @@
 "use client";
-import { sidebarMenu } from "@/constants/sidebar";
 import { Link, usePathname } from "@/navigation";
 import { useEffect, useMemo, useRef, useState } from "react";
-import { MdKeyboardArrowDown, MdMenu } from "react-icons/md";
+import { MdMenu } from "react-icons/md";
+import MenuButton from "./menu-button";
+import { useSidebarMenu } from "@/constants/sidebar";
+import Accordion from "@/components/common/accordion";
 
 const publicRoutes = ["/"];
 
@@ -15,11 +17,23 @@ export default function Sidebar({ children }: { children: React.ReactNode }) {
   const [isScrolled, setIsScrolled] = useState(false);
   const mainContentRef = useRef<HTMLDivElement>(null);
   const pathname = usePathname();
+  const { mainMenu, subMenu } = useSidebarMenu();
 
-  const currentPageTitle = useMemo(() => {
-    const currentMenuItem = sidebarMenu.find((item) => item.path === pathname);
-    return currentMenuItem ? currentMenuItem.label : "Dashboard";
-  }, [pathname]);
+  const findMenuLabel = (menus: any) => {
+    const menu = menus.find((menu: any) => menu.path === pathname);
+    if (menu) return menu.label;
+
+    const subMenu = menus
+      .flatMap((menu: any) => menu.sub_menu || [])
+      .find((sub: any) => sub.path === pathname);
+    return subMenu ? subMenu.label : null;
+  };
+
+  const currentPageTitle = useMemo(
+    () => findMenuLabel(mainMenu) || findMenuLabel(subMenu) || "Dashboard",
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [pathname, mainMenu, subMenu]
+  );
 
   useEffect(() => {
     const handleScroll = () => {
@@ -44,6 +58,24 @@ export default function Sidebar({ children }: { children: React.ReactNode }) {
     return <>{children}</>;
   }
 
+  const renderMenuItem = (menu: any, isSubMenu = false) => {
+    const IconComponent = menu.icon;
+    const isActive = pathname === menu.path;
+    const baseClasses = `${isSubMenu ? "py-4 px-10 flex flex-col" : "py-3 px-5 flex items-center gap-8" } text-[#280d5f] cursor-pointer transition-colors duration-300`;
+    const activeClasses = isActive ? "bg-[#7645D914] hover:bg-[#7645d91e]" : "hover:bg-gray-100";
+
+    return (
+      <Link
+        key={menu.key}
+        href={menu.path}
+        className={`${baseClasses} ${activeClasses}`}
+      >
+        {!isSubMenu && <IconComponent className="w-7 h-7 text-[#783cf1]" />}
+        {menu.label}
+      </Link>
+    );
+  };
+
   return (
     <div className="flex h-screen overflow-hidden">
       <aside
@@ -53,34 +85,27 @@ export default function Sidebar({ children }: { children: React.ReactNode }) {
           lg:relative lg:translate-x-0
         `}
       >
-        <header className="py-4 lg:py-5 px-4 text-[#280d5f] text-lg font-semibold">
+        <header className="py-4 lg:py-7 px-4 text-[#280d5f] text-lg font-semibold">
           Tabshier Hub
         </header>
-        <section>
-          <div className="px-2">
-            <button className="flex justify-between items-center bg-[#faf9fa] w-full p-5 text-[#280d5f] text-sm font-semibold rounded-full">
-              SiamOutlet <MdKeyboardArrowDown className="w-7 h-7" />
-            </button>
-          </div>
+        <MenuButton />
+        <section className="overflow-y-auto max-h-[calc(100vh-100px)] pb-14 hide-scrollbar">
           <ul className="py-2 w-full">
-            {sidebarMenu.map((item) => {
-              const IconComponent = item.icon;
-              const isActive = pathname === item.path;
-              return (
-                <Link
-                  key={item.key}
-                  href={item.path}
-                  className={`py-3 px-5 flex items-center gap-8 text-[#280d5f] cursor-pointer transition-colors duration-300 ${
-                    isActive
-                      ? "bg-[#7645D914] hover:bg-[#7645d91e]"
-                      : "hover:bg-gray-100 "
-                  }`}
-                >
-                  <IconComponent className="w-7 h-7 text-[#783cf1]" />{" "}
-                  {item.label}
-                </Link>
-              );
-            })}
+            {mainMenu.map((menu) => renderMenuItem(menu))}
+            {subMenu.map((menu) => (
+              <Accordion
+                key={menu.key}
+                className="px-5 py-3 hover:bg-gray-100"
+                title={
+                  <div className="flex items-center gap-8">
+                    <menu.icon className="w-7 h-7 text-[#783cf1]" />{" "}
+                    {menu.label}
+                  </div>
+                }
+              >
+                {menu.sub_menu.map((sub) => renderMenuItem(sub, true))}
+              </Accordion>
+            ))}
           </ul>
         </section>
       </aside>
@@ -99,7 +124,7 @@ export default function Sidebar({ children }: { children: React.ReactNode }) {
               onClick={() => setIsSidebarOpen(!isSidebarOpen)}
               className="focus:outline-none lg:hidden"
             >
-              <MdMenu className="w-6 h-6 text-[#7645d9]" />
+              <MdMenu className="w-6 h-6" />
             </button>
             <h1 className="text-2xl font-bold text-[#7645d9] lg:text-3xl">
               {currentPageTitle}

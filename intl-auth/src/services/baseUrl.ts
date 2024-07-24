2d40294c4fc9f933b07a4f3301a08b900cc01dfc
Yotsatorn  Pathan <120444604+R-nasPT@users.noreply.github.com>
2024-07-24 10:12:47 +0700

    Create baseUrl.ts

---
+++ intl-auth/src/services/baseUrl.ts
@@ -0,0 +1,53 @@
+import axios from "axios";
+import { getToken } from "next-auth/jwt";
+import { useMemo } from "react";
+
+//สามารถ ใส่ token ได้หลายวิธี
+
+export const axiosInstance = axios.create({
+  baseURL: process.env.IFP_HOST,
+  headers: {
+    "Content-Type": "application/json",
+    // Authorization: `Bearer ${process.env.IFP_TOKEN}`,
+    // Authorization: `Bearer 1pzYTQUJynEjFOesdRULww0ViD8KX7-Pm5K9InXrL7I5xEtfsqkHru8xrSXM7jClXdiPW33ILfY9bLvNEGOD1lorOEZhjmLevTYijvhg5R-oYqafMrBI1NMHGTVgK7EbmA7l6V`,
+  },
+});
+
+export function createAxiosInstance(token?: string) {
+
+  // if (token) {
+  //   instance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  // }
+
+  axiosInstance.interceptors.request.use(
+    async (config) => {
+      console.log(11111111111111111111);
+      console.log(token);
+
+      if (token) {
+        config.headers["Authorization"] = `Bearer ${token}`;
+      }
+
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+  axiosInstance.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      if (!error.response) {
+        console.error("Network error or server not responding");
+      } else {
+        console.error(
+          `API error: ${error.response.status} - ${error.response.statusText}`
+        );
+      }
+      return Promise.reject(error);
+    }
+  );
+
+  return axiosInstance;
+}

66d47f56130e28b0c6337820768d1e3fae48ff24
Yotsatorn  Pathan <120444604+R-nasPT@users.noreply.github.com>
2024-07-24 10:15:47 +0700

    Update baseUrl.ts

--- intl-auth/src/services/baseUrl.ts
+++ intl-auth/src/services/baseUrl.ts
@@ -24,6 +24,7 @@ export function createAxiosInstance(token?: string) {
 
       if (token) {
         config.headers["Authorization"] = `Bearer ${token}`;
+        //config.headers.Authorization = `Bearer ${token}`;
       }
 
       return config;

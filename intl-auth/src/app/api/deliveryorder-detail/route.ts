ba3b4ff0b74743b2a1572c3acd991b8459037809
Yotsatorn  Pathan <120444604+R-nasPT@users.noreply.github.com>
2024-08-01 09:33:05 +0700

    Update route.ts

--- intl-auth/src/app/api/deliveryorder-detail/route.ts
+++ intl-auth/src/app/api/deliveryorder-detail/route.ts
@@ -9,13 +9,49 @@ export async function GET(request: Request) {
   const session = await getSession();
   const axiosInstance = configureAxiosWithToken((session?.user as any)?.token);
 
+  // คำนวณวันที่ของสามปีที่แล้ว
+  const threeYearsAgo = new Date();
+  threeYearsAgo.setFullYear(threeYearsAgo.getFullYear() - 3);
+  threeYearsAgo.setMonth(0); // เดือนมกราคม
+  threeYearsAgo.setDate(1); // วันที่ 1
+  threeYearsAgo.setHours(0, 0, 0, 0); // เวลา 00:00:00.000
+
+  // คำนวณวันที่สองปีที่แล้ว
+  const twoYearsAgo = new Date();
+  twoYearsAgo.setFullYear(twoYearsAgo.getFullYear() - 2);
+
+  // คำนวณวันที่ปีที่ผ่านมา
+  const lastYear = new Date();
+  lastYear.setFullYear(lastYear.getFullYear() - 1);
+  lastYear.setMonth(0); // เดือนมกราคม
+  lastYear.setDate(1); // วันที่ 1
+  lastYear.setHours(0, 0, 0, 0); // เวลา 00:00:00.000
+
+  // คำนวณวันที่สิ้นสุดของปีที่ผ่านมา
+  const endOfLastYear = new Date();
+  endOfLastYear.setFullYear(endOfLastYear.getFullYear() - 1);
+  endOfLastYear.setMonth(11); // เดือนธันวาคม
+  endOfLastYear.setDate(31); // วันที่ 31
+  endOfLastYear.setHours(23, 59, 59, 999); // เวลา 23:59:59.999
+
+  // คำนวณวันที่เริ่มต้นของปีนี้
+  const startOfYear = new Date();
+  startOfYear.setMonth(0); // เดือนมกราคม
+  startOfYear.setDate(1); // วันที่ 1
+  startOfYear.setHours(0, 0, 0, 0); // เวลา 00:00:00.000
+
+  // วันที่ปัจจุบัน
+  const currentDate = new Date();
+
   try {
     const response = await axiosInstance.get("/deliveryorder", {
       params: {
+        $filter: `updatedDate ge ${twoYearsAgo.toISOString()} and updatedDate le ${currentDate.toISOString()}`,
         $select:
           "id,accountId,code,reference,customerName,courierTrackingCode,status,courierId,itemCount,printCount,updatedDate,",
         $expand:
           "account,courier,items($select=amount;$expand=sku($select=info;$expand=info($select=onHand))),attachments",
+        $orderby: "updatedDate desc", //<-- จะใช้ได้ก็ต่อเมื่อข้อมูลไม่เยอะเกินไป
       },
     });
 

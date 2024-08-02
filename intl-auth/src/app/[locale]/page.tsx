e2eb5c03d31720d65288829fb1c12075bc3670cc
Yotsatorn  Pathan <120444604+R-nasPT@users.noreply.github.com>
2024-08-02 17:20:51 +0700

    Update page.tsx

--- intl-auth/src/app/[locale]/page.tsx
+++ intl-auth/src/app/[locale]/page.tsx
@@ -1,14 +1,22 @@
 import Login from "@/components/auth/login";
 import { getTranslations } from "next-intl/server";
-import { withLocale } from '@/utils/withLocale';
+import { redirect } from "next/navigation";
 
-async function Home() {
-  const t = await getTranslations("INDEX");
+export default async function RootPage({
+  params,
+  searchParams,
+}: {
+  params: { locale: string };
+  searchParams: { page?: string };
+}) {
+  // const t = await getTranslations("INDEX");
+  if (!searchParams.page || searchParams.page !== "login") {
+    redirect(`/${params.locale}/?page=login`);
+  }
+
   return (
     <>
       <Login />
     </>
   );
 }
-
-export default withLocale(Home);

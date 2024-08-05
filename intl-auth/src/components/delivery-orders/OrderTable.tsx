8b93bb186d1a17544a100e0198894ed3d4425908
Yotsatorn  Pathan <120444604+R-nasPT@users.noreply.github.com>
2024-08-05 15:56:53 +0700

    Update OrderTable.tsx

--- intl-auth/src/components/delivery-orders/OrderTable.tsx
+++ intl-auth/src/components/delivery-orders/OrderTable.tsx
@@ -6,27 +6,29 @@ import { formatDateLong, formatTime } from "@/utils";
 import OederListSkeleton from "./OrderListSkeleton";
 import StatusBadge from "./StatusBadge";
 import OrderMobileSkeleton from "./OrderMobileSkeleton";
+import { usePrefetchOrderId } from "@/services";
 
 interface OrderTableProps {
   orders: DeliveryOrderListTypes[];
   isLoading: boolean;
-  isFetching: boolean;
-  openDrawer: () => void;
+  openDrawer: (orderId: string) => void;
 }
 
-export default function OrderTable({ orders, isLoading, isFetching, openDrawer }: OrderTableProps) {
+export default function OrderTable({ orders, isLoading, openDrawer }: OrderTableProps) {
   const {
-handleSelectAll,
-handleSelectItem,
-isAllSelected,
-selectedCount,
-selectedItems,
-} = useCheckbox(orders);
+    handleSelectAll,
+    handleSelectItem,
+    isAllSelected,
+    selectedCount,
+    selectedItems,
+  } = useCheckbox(orders);
+
+  const prefetchOrderData = usePrefetchOrderId()
 
   return (
     <>
       {selectedCount > 0 && (
-        <div className="bg-[#eee8fa] py-5 px-3">
+        <div className="bg-[#eee8fa] py-5 px-3 my-3 lg:my-0">
           Selected {selectedCount} item(s)
         </div>
       )}
@@ -50,7 +52,7 @@ selectedItems,
           </tr>
         </thead>
         <tbody>
-          {isLoading || isFetching ? (
+          {isLoading ? (
             <OederListSkeleton />
           ) : (
             orders.map((order) => (
@@ -61,7 +63,8 @@ selectedItems,
                     ? "bg-[#eee8fa] hover:bg-[#e3d6ff]"
                     : "hover:bg-gray-100"
                 }`}
-                onClick={openDrawer}
+                onMouseEnter={() => prefetchOrderData(order.id)}
+                onClick={() => openDrawer(order.id)}
               >
                 <td className="p-1" onClick={(e) => e.stopPropagation()}>
                   <Checkbox
@@ -84,7 +87,9 @@ selectedItems,
                   <p className="font-semibold">{order.courier?.name}</p>
                   <p className="text-[#846eae]">{order.courierTrackingCode}</p>
                 </td>
-                <td className="p-4">520 / {order.itemCount}</td>
+                <td className="p-4 text-center">
+                  {order.cod === 0 ? "-" : order.cod} / {order.itemCount}
+                </td>
                 <td className="p-4">
                   <p>{formatDateLong(order.updatedDate)}</p>
                   <p>เวลา {formatTime(order.updatedDate)}</p>
@@ -92,35 +97,13 @@ selectedItems,
               </tr>
             ))
           )}
-          <tr className={`border-b border-[#e0e0e0]`}>
-            <td className="p-1">
-              <Checkbox id={`row-1`} />
-            </td>
-            <td className="p-4 flex flex-col gap-1 items-center">
-              <div className="flex items-center gap-2 border border-pink-500 text-pink-700 text-[13px] cursor-pointer hover:bg-gray-100 pl-1 pr-2 py-[.5px] w-fit rounded-full transition-colors duration-300">
-                <div className="p-[6px] bg-pink-400 rounded-full"></div>
-                <span>Pending </span>
-              </div>
-              <p className="text-[13px] text-[#7c70ab]">2407-0G4-01643</p>
-            </td>
-            <td className="p-4">
-              <p>PO-202407025</p>
-              <p>คุณ เจน ฟหกะ</p>
-            </td>
-            <td className="p-4 whitespace-nowrap">
-              <p>TikTok Pickup (เจ้าหน้าที่มารับที่โกดัง)</p>
-              <p className="text-[#846eae]">TH01105WT86K9E</p>
-            </td>
-            <td className="p-4">- / 0</td>
-            <td className="p-4">Thu 11 Jul 2024 09:37:17</td>
-          </tr>
         </tbody>
       </table>
 
       {/* ----- Mobile ----- */}
       <table className="w-full lg:hidden text-sm">
         <tbody>
-          {isLoading || isFetching ? (
+          {isLoading ? (
             <OrderMobileSkeleton />
           ) : (
             orders.map((order) => (
@@ -129,7 +112,8 @@ selectedItems,
                 className={`border-b border-[#e0e0e0] ${
                   selectedItems[order.id] ? "bg-[#eee8fa]" : ""
                 }`}
-                onClick={openDrawer}
+                onMouseEnter={() => prefetchOrderData(order.id)}
+                onClick={() => openDrawer(order.id)}
               >
                 <td className="py-4 flex gap-1 md:gap-4">
                   <div

3e5e21d48e3abb580580574633f05d69cb3a2709
Yotsatorn  Pathan <120444604+R-nasPT@users.noreply.github.com>
2024-08-09 13:12:07 +0700

    Update route.ts

--- intl-auth/src/app/api/delivery-list/print/route.ts
+++ intl-auth/src/app/api/delivery-list/print/route.ts
@@ -4,6 +4,8 @@ export async function POST(req: Request) {
   // แปลงข้อมูลเป็น JSON (ถ้าข้อมูลเป็น JSON)
   const orderId = JSON.parse(rawData);
   console.log(orderId.printId[0]);
+  const { printId } = JSON.parse(rawData);
+    console.log("Received IDs:", printId);
 
   return Response.json({ message: "ได้รับข้อมูลแล้ว" });
 }

7e43d3fe7dc71a8ec484385e6e80de6ec6a5e7c1
Yotsatorn  Pathan <120444604+R-nasPT@users.noreply.github.com>
2024-08-21 16:48:23 +0700

    Update RealDeliveryOrdersContent.tsx

--- intl-auth/src/components/delivery-orders/RealDeliveryOrdersContent.tsx
+++ intl-auth/src/components/delivery-orders/RealDeliveryOrdersContent.tsx
@@ -1,43 +1,71 @@
 "use client";
 
 import OrderTable from "./OrderTable";
-import Pagination from "../ui/pagination";
 import { useRouter } from "@/navigation";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useDeliveryOrdersList } from "@/services";
 import DrawerOrderDetail from "./DrawerOrderDetail";
 import OrderStatusFilter from "./OrderStatusFilter";
+import CancelDialog from "../common/modal/CancelDialog";
+import { Pagination } from "../ui";
+import OrderSearch from "./OrderSearch";
 
 interface DeliveryOrdersContentProps {
   page: number;
   per_page: number;
   status?: string;
+  merchant?: string;
+  search?: string;
+  field?: string;
 }
+
 export default function DeliveryOrdersContent({
   page: initialPage,
   per_page: initialPerPage,
   status: initialStatus,
+  merchant: initialMerchant,
+  search: initialSearch,
+  field: initialField,
 }: DeliveryOrdersContentProps) {
   const router = useRouter();
-  const [page, setPage] = useState(initialPage);
-  const [perPage, setPerPage] = useState(initialPerPage);
-  const [status, setStatus] = useState(initialStatus);
   const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false);
   const [selectedOrderId, setSelectedOrderId] = useState<string | null>(null);
+  const [openCancel, setOpenCancel] = useState(false);
+  const [cancelOrderId, setCancelOrderId] = useState<string[]>([]);
+  const [filters, setFilters] = useState({
+    page: initialPage,
+    per_page: initialPerPage,
+    status: initialStatus,
+    merchant: initialMerchant,
+    search: initialSearch,
+    field: initialField
+  });
 
-  const { data: orders, isLoading, isFetching } = useDeliveryOrdersList(initialPage, initialPerPage, initialStatus);
+  const { data: orders, isLoading, isFetching, refetch } = useDeliveryOrdersList( 
+    initialPage, initialPerPage, initialStatus, initialMerchant, initialSearch, initialField
+  );
 
-  const handleStatusChange = (newStatus: string) => {
-    setStatus(newStatus);
-    router.push(
-      `/delivery-orders?page=1&per_page=${perPage}${
-        newStatus !== "" ? `&status=${newStatus}` : ""
-      }`
-    );
+  const updateURL = useCallback(() => {
+    const params = new URLSearchParams();
+    if (filters.search) params.append("search", filters.search);
+    if (filters.field) params.append("field", filters.field);
+    if (filters.page) params.append("page", filters.page.toString());
+    if (filters.per_page) params.append("per_page", filters.per_page.toString());
+    if (filters.status) params.append("status", filters.status);
+    if (filters.merchant) params.append("merchant", filters.merchant);
+    router.push(`/delivery-orders?${params.toString()}`);
+  }, [filters, router]);
+
+  const handleChange = (key: keyof typeof filters, value: string | number) => {
+    setFilters(prev => ({
+      ...prev,
+      [key]: value,
+      page: 1,
+    }));
   };
 
-  const start = (page - 1) * perPage;
-  const end = start + perPage;
+  const start = (filters.page - 1) * filters.per_page;
+  const end = start + filters.per_page;
 
   const entries = orders?.data || [];
   const totalCount = orders?.total || 0;
@@ -47,17 +75,30 @@ export default function DeliveryOrdersContent({
     setIsDrawerOpen(true);
   };
 
+  const handleOpenCancel = (orderId: string | string[]) => {
+    if (Array.isArray(orderId)) {
+      setCancelOrderId(orderId);
+    } else {
+      setCancelOrderId([orderId]);
+    }
+    setOpenCancel(true);
+  };
+
   useEffect(() => {
-    if (initialPage !== page) setPage(initialPage);
-    if (initialPerPage !== perPage) setPerPage(initialPerPage);
-    if (initialStatus !== status) setStatus(initialStatus);
-  }, [initialPage, initialPerPage, initialStatus, page, perPage, status]);
+    updateURL();
+  }, [updateURL]);
 
   return (
     <>
+      <OrderSearch
+        handleMerchantChange={value => handleChange("merchant", value)}
+        handleSearchChange={value => handleChange("search", value)}
+        handleFieldChange={value => handleChange("field", value)}
+      />
+
       <OrderStatusFilter
-        status={status}
-        handleStatusChange={handleStatusChange}
+        status={filters.status}
+        handleStatusChange={value => handleChange("status", value)}
         statusResponse={orders?.status!}
       />
 
@@ -65,19 +106,29 @@ export default function DeliveryOrdersContent({
         orders={entries}
         isLoading={isLoading || isFetching}
         openDrawer={handleOpenDrawer}
-        currentStatus={status}
+        currentStatus={filters.status}
+        openCancel={handleOpenCancel}
       />
 
       <Pagination
         total={totalCount}
         hasNextPage={end < totalCount}
         hasPrevPage={start > 0}
+        onPerPageChange={value => handleChange("per_page", value)}
       />
 
       <DrawerOrderDetail
         isOpen={isDrawerOpen}
         onClose={() => setIsDrawerOpen(false)}
         orderId={selectedOrderId}
+        openCancel={handleOpenCancel}
+      />
+
+      <CancelDialog
+        refetch={refetch}
+        open={openCancel}
+        onClose={() => setOpenCancel(false)}
+        orderId={cancelOrderId}
       />
     </>
   );

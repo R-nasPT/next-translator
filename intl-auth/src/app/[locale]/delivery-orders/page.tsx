d5f750bbbd0c742703a9c0b3ca26ef84e13fc61a
Yotsatorn  Pathan <120444604+R-nasPT@users.noreply.github.com>
2024-08-21 16:54:45 +0700

    Update delivery order page

--- intl-auth/src/app/[locale]/delivery-orders/page.tsx
+++ intl-auth/src/app/[locale]/delivery-orders/page.tsx
@@ -14,65 +14,32 @@ interface DeliveryOrdersProps {
 export default async function DeliveryOrders({
   searchParams,
 }: DeliveryOrdersProps) {
-  const page = Number(searchParams["page"] ?? "1");
-  const per_page = Number(searchParams["per_page"] ?? "10");
+  // const page = Number(searchParams["page"] ?? "1");
+  // const per_page = Number(searchParams["per_page"] ?? "10");
+
+  const page = Number(searchParams["page"]);
+  const per_page = Number(searchParams["per_page"]);
+  const status = searchParams["status"] as string | undefined;
+  const merchant = searchParams["merchant"] as string | undefined;
+  const search = searchParams["search"] as string | undefined;
+  const field = searchParams["field"] as string | undefined;
+
+  if (!page || !per_page) {
+    redirect("/delivery-orders?page=1&per_page=10");
+  }
 
   return (
     <div className="bg-white rounded-3xl p-6 shadow-[0_0_3px_rgba(0,0,0,0.3)] mb-5">
-      <section className="flex justify-between items-center gap-5">
-        <input
-          className="px-5 py-2 rounded-full border border-[#e0e0e0] focus:outline-none bg-[#fafafa] flex-1"
-          type="text"
-          placeholder="Search"
-        />
-        <input
-          className="hidden lg:block px-5 py-2 rounded-full border border-[#e0e0e0] focus:outline-none bg-[#fafafa] flex-1"
-          type="date"
-        />
-        <h1 className="hidden lg:block">To</h1>
-        <input
-          className="hidden lg:block px-5 py-2 rounded-full border border-[#e0e0e0] focus:outline-none bg-[#fafafa] flex-1"
-          type="date"
-        />
-        <input
-          className="hidden lg:block px-5 py-2 rounded-full border border-[#e0e0e0] focus:outline-none bg-[#fafafa] flex-1"
-          type="text"
-        />
-      </section>
+      <DeliveryOrderToolbar />
 
-      <section className="hidden lg:flex py-5 gap-3 text-[13px] text-[#280d5f]">
-        <span className="cursor-pointer hover:bg-gray-100 px-2 py-1 rounded-full transition-colors duration-300">
-          All Status (312)
-        </span>
-        <div className="flex items-center gap-2 cursor-pointer hover:bg-gray-100 px-2 py-1 rounded-full transition-colors duration-300">
-          <div className="p-[6px] bg-[#979797] rounded-full"></div>
-          <span>Draft (0)</span>
-        </div>
-        <div className="flex items-center gap-2 cursor-pointer hover:bg-gray-100 px-2 py-1 rounded-full transition-colors duration-300">
-          <div className="p-[6px] bg-yellow-400 rounded-full"></div>
-          <span>Pending (20)</span>
-        </div>
-        <div className="flex items-center gap-2 cursor-pointer hover:bg-gray-100 px-2 py-1 rounded-full transition-colors duration-300">
-          <div className="p-[6px] bg-blue-400 rounded-full"></div>
-          <span>Printed (17)</span>
-        </div>
-        <div className="flex items-center gap-2 cursor-pointer hover:bg-gray-100 px-2 py-1 rounded-full transition-colors duration-300">
-          <div className="p-[6px] bg-purple-400 rounded-full"></div>
-          <span>Picked (35)</span>
-        </div>
-        <div className="flex items-center gap-2 cursor-pointer hover:bg-gray-100 px-2 py-1 rounded-full transition-colors duration-300">
-          <div className="p-[6px] bg-pink-400 rounded-full"></div>
-          <span>Packed (335)</span>
-        </div>
-        <div className="flex items-center gap-2 cursor-pointer hover:bg-gray-100 px-2 py-1 rounded-full transition-colors duration-300">
-          <div className="p-[6px] bg-green-400 rounded-full"></div>
-          <span>Dispatched (565)</span>
-        </div>
-        <div className="flex items-center gap-2 cursor-pointer hover:bg-gray-100 px-2 py-1 rounded-full transition-colors duration-300">
-          <div className="p-[6px] bg-red-500 rounded-full"></div>
-          <span>Cancelled (46)</span>
-        </div>
-      </section>
+      <DeliveryOrdersContent
+        page={page}
+        per_page={per_page}
+        status={status}
+        merchant={merchant}
+        search={search}
+        field={field}
+      />
 
       {/* <Suspense fallback={<OrderListSkeleton />}> */}
         <DeliveryOrdersContent page={page} per_page={per_page} />

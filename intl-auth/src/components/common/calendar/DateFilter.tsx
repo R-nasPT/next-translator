e8a59da21ade9da30506c1b7008d75070d0d2541
Yotsatorn  Pathan <120444604+R-nasPT@users.noreply.github.com>
2024-08-30 10:27:42 +0700

    Update DateFilter.tsx

--- intl-auth/src/components/common/calendar/DateFilter.tsx
+++ intl-auth/src/components/common/calendar/DateFilter.tsx
@@ -42,7 +42,7 @@ export default function DateFilter({ onDateChange }: DateFilterProps) {
 
   return (
     <Datepicker
-      // showShortcuts
+      showShortcuts
       useRange={false}
       primaryColor={"purple"}
       value={value}

f22072cc6192e4df2da9fdb09de0fe049655c629
Yotsatorn  Pathan <120444604+R-nasPT@users.noreply.github.com>
2024-09-04 10:25:32 +0700

    Create routes

---
+++ intl-auth/src/routes/index.ts
@@ -0,0 +1,15 @@
+export const publicRoutes = ["/"];
+export const showNavbarPaths = [
+  "/dashboard",
+  "/delivery-orders",
+  "/advices",
+  "/location",
+  "/rack",
+  "/zone",
+  "/settings",
+];
+
+export const showHeaderNavPaths = [
+  "/location/create",
+  "/delivery-order/create",
+];

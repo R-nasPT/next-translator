82929fd43d8ac2ece0b4d58db108db2f260e326e
Yotsatorn  Pathan <120444604+R-nasPT@users.noreply.github.com>
2024-10-14 20:25:01 +0700

    Update tailwind.config.ts

--- intl-auth/tailwind.config.ts
+++ intl-auth/tailwind.config.ts
@@ -1,21 +1,29 @@
 import type { Config } from "tailwindcss";
+import { PluginAPI } from "tailwindcss/types/config";
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/constants/**/*.{js,ts,jsx,tsx}", //<--เราสามารถเพิ่ม folder ที่อยากให้ tailwind ทำงานได้
+    "./src/constants/**/*.{js,ts,jsx,tsx}",
+    "./src/documents/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/react-tailwindcss-datepicker/dist/index.esm.{js,ts}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }: PluginAPI) {
+      addUtilities({
+        '.transition-smooth': {
+          transitionProperty: 'all',
+          transitionTimingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
+          transitionDuration: '300ms',
+        },
+      });
+    },
+  ],
 };
 export default config;
